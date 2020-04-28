a90628bbce6819b9ac454f9839605637ea758f76
Faraj Farook <farajfarook@gmail.com>
2020-04-28 20:37:32 +1000

    Change pin layout

--- src/pins.h
+++ src/pins.h
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#define READ_WRITE PB10
+#define READ_WRITE PB1
 #define CLK_INTR PB11
 
 #define ADDR_LEN 16
@@ -8,33 +8,33 @@
 
 int DATA[DATA_LEN] = {
     //D7-D4
+    PA1,
     PA2,
     PA3,
     PA4,
-    PA5,
     //D3-D0
+    PA5,
     PA6,
     PA7,
     PB0,
-    PB1,
 };
 
 int ADDR[ADDR_LEN] = {
     //A15-A12
-    PA1,
     PA0,
     PC15,
     PC14,
+    PC13,
     //A11-A8
+    PB9,
+    PB8,
     PB7,
     PB6,
+    //A7-A4
     PB5,
     PB4,
-    //A7-A4
     PB3,
     PA15,
-    PA12,
-    PA11,
     //A3-A0
     PB15,
     PB14,
